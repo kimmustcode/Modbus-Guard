c1adf580e81a315adaf513dd29a3e67aa63fc3de
Kim <sangwookim1999@gmail.com>
2026-05-28 09:44:45 -0700

    yaml load to dict

--- src/py_engine/engine.py
+++ src/py_engine/engine.py
@@ -3,6 +3,9 @@ import yaml
 class PolicyEngine:
     def __init__(self, rules_path):
         # TODO: Load rules from YAML
+        with open(rules_path, "r") as rules: 
+            self.rules_dict = yaml.safe_load(file)
+            print(self.rules_dict)
         pass
 
     def evaluate(self, packet):
